8bb0698e3093109e916a5b67a1afb218f3836897
Omar Lo Giudice <146293874+FearXz@users.noreply.github.com>
2023-11-15 10:44:24 +0100

    star workano

---
+++ bw1_f0623b_gruppo8_13_11_2023/assets/js/feedback.js
@@ -0,0 +1,56 @@
+function createStars() {
+    let starContainer = document.getElementById('rating');
+    for (let i = 0; i < 10; i++){
+        starContainer.innerHTML+=`<img src="assets/img/star.svg" alt="stellina rating" class="opacityStar">`;
+    }
+}
+
+function feedbackStars() {
+    let stars = document.querySelectorAll("#rating img");
+    let clicked = false;
+
+    stars.forEach(function (star, index) {
+        star.addEventListener("click", function () {
+            clicked = true;
+
+            // Rimuovi la classe "opacityStar" da tutte le stelle
+            stars.forEach(function (s) {
+                s.classList.add("opacityStar");
+            });
+
+            // Aggiungi la classe "opacityStar" solo alle stelle fino a quella cliccata
+            for (let i = 0; i <= index; i++) {
+                stars[i].classList.remove("opacityStar");
+            }
+        });
+
+        star.addEventListener("mouseover", function () {
+            if (!clicked) {
+                // Se non è stato cliccato, colora la stella e le precedenti
+                for (let i = 0; i <= index; i++) {
+                    stars[i].classList.remove("opacityStar");
+                }
+            }
+        });
+
+        star.addEventListener("mouseout", function () {
+            if (!clicked) {
+                // Se non è stato cliccato, reimposta tutte le stelle a trasparenti
+                stars.forEach(function (s) {
+                    s.classList.add("opacityStar");
+                });
+            }
+        });
+    });
+}
+
+
+
+
+
+
+
+window.onload = function () {
+    createStars();
+    feedbackStars();
+}
